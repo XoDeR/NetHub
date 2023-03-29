816a2f51fb562caadb00a58a7432869957ba7d5c
Volodymyr <XoDeR@users.noreply.github.com>
2023-03-29 17:29:01 +0200

    Client. Nav. Updated with using Link from next.js

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,30 +1,22 @@
+import Link from "next/link";
+
 const Nav = () => {
   return (
     <ul className="nav justify-content-center">
       <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="#">
-          Active
-        </a>
+        <Link href="/" className="nav-link active">
+          <a>Home</a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">
-          Link
-        </a>
+        <Link href="/login" className="nav-link active">
+          <a>Login</a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">
-          Link
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          className="nav-link disabled"
-          href="#"
-          tabIndex={-1}
-          aria-disabled="true"
-        >
-          Disabled
-        </a>
+        <Link href="/register" className="nav-link active">
+          <a>Register</a>
+        </Link>
       </li>
     </ul>
   );
