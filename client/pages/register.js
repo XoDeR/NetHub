ddc4bc1f26b77106c3b756fe5e0e9f6ec2f5a218
Volodymyr <XoDeR@users.noreply.github.com>
2023-04-05 14:56:28 +0200

    Added loading state and icon to Register page

--- client/pages/register.js
+++ client/pages/register.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { Modal } from "antd";
 import Link from "next/link";
+import { SyncOulined } from "@ant-design/icons";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -10,11 +11,13 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [secret, setSecret] = useState("");
   const [ok, setOk] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     // console.log(name, email, password, secret);
     try {
+      setLoading(true);
       const { data } = await axios.post(
         `${process.env.NEXT_PUBLIC_API}/register`,
         {
@@ -29,8 +32,10 @@ const Register = () => {
       setPassword("");
       setSecret("");
       setOk(data.ok);
+      setLoading(false);
     } catch (err) {
       toast.error(err.response.data);
+      setLoading(false);
     }
   };
 
@@ -114,7 +119,7 @@ const Register = () => {
                 disabled={!name || !email || !password || !secret}
                 className="btn btn-primary col-12"
               >
-                Submit
+                {loading ? <SyncOulined spin className="py-1" /> : "Submit"}
               </button>
             </div>
           </form>
