873cfd194cc15087efdbd5decd8d20db3befe816
Volodymyr <XoDeR@users.noreply.github.com>
2023-04-07 19:22:46 +0200

    Server: Added the currentUser endpoint to check the token for protected pages in frontend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-import { register, login } from "../controllers/auth";
+import { requireSignIn } from "../middlewares";
+
+import { register, login, currentUser } from "../controllers/auth";
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/current-user", requireSignIn, currentUser);
 
 module.exports = router;
