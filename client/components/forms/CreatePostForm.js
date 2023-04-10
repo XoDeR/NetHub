23763bf6cef9dd2b2829f5e0f2a0de233f38918f
Volodymyr <XoDeR@users.noreply.github.com>
2023-04-10 18:51:54 +0200

    Added rich text editor react-quill

--- client/components/forms/CreatePostForm.js
+++ client/components/forms/CreatePostForm.js
@@ -1,16 +1,21 @@
 import { Avatar } from "antd";
+import dynamic from "next/dynamic";
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false }); // dynamic import
+// import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const CreatePostForm = ({ content, setContent, postSubmit }) => {
   return (
     <div className="card">
       <div className="card-body pb-3">
         <form className="form-group">
-          <textarea
+          <ReactQuill
+            theme="snow"
             value={content}
-            onChange={(e) => setContent(e.target.value)}
+            onChange={(e) => setContent(e)}
             className="form-control"
             placeholder="Write something..."
-          ></textarea>
+          />
         </form>
       </div>
 
