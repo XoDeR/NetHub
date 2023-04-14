940406fd32760ab781c01009307d979b4792f59d
Volodymyr <XoDeR@users.noreply.github.com>
2023-04-14 19:49:16 +0200

    Added bootstrap DropDown and also bootstrap js to handle DropDown

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -32,6 +32,36 @@ const Nav = () => {
         </a>
       </Link>
 
+      <div className="dropdown">
+        <a
+          className="btn btn-secondary dropdown-toggle"
+          href="#"
+          role="button"
+          id="dropdownMenuLink"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Dropdown link
+        </a>
+        <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <li>
+            <a className="dropdown-item" href="#">
+              Action
+            </a>
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Another action
+            </a>
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Something else here
+            </a>
+          </li>
+        </ul>
+      </div>
+
       {state !== null ? (
         <>
           <Link href="/user/dashboard">
