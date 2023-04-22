f161d3370b64ed1bff0d95869c8d8639d15a8800
Volodymyr <XoDeR@users.noreply.github.com>
2023-04-22 15:06:04 +0200

    Client: added Following page

---
+++ client/pages/user/following.js
@@ -0,0 +1,74 @@
+import { useContext, useState, useEffect } from "react";
+import { Avatar, List } from "antd";
+import moment from "moment";
+import { useRouter } from "next/router";
+import { UserContext } from "../../context";
+import axios from "axios";
+import { RollbackOutlined } from "@ant-design/icons";
+import Link from "next/link";
+
+const Following = () => {
+  const [state] = useContext(UserContext);
+  const [people, setPeople] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (state && state.token) fetchFollowing();
+  }, [state && state.token]);
+
+  const fetchFollowing = async () => {
+    try {
+      const { data } = await axios.get("/user-following");
+      console.log("following =>", data);
+      setPeople(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const imageSource = (user) => {
+    if (user.image) {
+      return user.image.url;
+    } else {
+      return "/images/user-avatar-default.jpg";
+    }
+  };
+
+  const handleUnfollow = async () => {
+    //
+  };
+
+  return (
+    <div className="row col-md-6 offset-md-3">
+      <List
+        itemLayout="horizontal"
+        dataSource={people}
+        renderItem={(user) => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={<Avatar src={imageSource(user)} />}
+              title={
+                <div className="d-flex justify-content-between">
+                  {user.username} {user.name}{" "}
+                  <span
+                    onClick={() => handleUnfollow(user)}
+                    className="text-primary pointer"
+                  >
+                    Unfollow
+                  </span>
+                </div>
+              }
+            />
+          </List.Item>
+        )}
+      />
+      <Link href="/user/dashboard">
+        <a className="d-flex justify-content-center pt-5">
+          <RollbackOutlined />
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+export default Following;
