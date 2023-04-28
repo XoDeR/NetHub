257978e0cba2cfe82cf0196a19c95d60455532c8
Volodymyr <XoDeR@users.noreply.github.com>
2023-04-28 14:45:20 +0200

    Home page with parallax effect

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,14 @@
 import { useContext } from "react";
 import { UserContext } from "../context";
+import ParallaxBg from "../components/cards/ParallaxBg";
 
 const Home = () => {
   const [state, setState] = useContext(UserContext);
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h1 className="display-1 text-center py-5">Home page</h1>
-          {JSON.stringify(state)}
-          <img src="/images/default.jpg" alt="image" />
-        </div>
-      </div>
-    </div>
+    <>
+      <ParallaxBg url="/images/default.jpg" />
+    </>
   );
 };
 
