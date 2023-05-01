41a109109860e7d5a94a4b0cdfcaafe9eba272e7
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-01 16:03:54 +0200

    Added meta information on the main page for SEO

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,12 +3,32 @@ import { UserContext } from "../context";
 import ParallaxBg from "../components/cards/ParallaxBg";
 import axios from "axios";
 import Post from "../components/cards/Post";
+import Head from "next/head";
 
 const Home = ({ posts }) => {
   const [state, setState] = useContext(UserContext);
 
+  const head = () => (
+    <Head>
+      <title>Social Network</title>
+      <meta name="description" content="A social network" />
+      <meta
+        property="og:description"
+        content="A social network for creative people"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="NetHub" />
+      <meta property="og:url" content="http://nethub.com" />
+      <meta
+        property="og:image:secure_url"
+        content="http://nethub.com/images/default.jpg"
+      />
+    </Head>
+  );
+
   return (
     <>
+      {head()}
       <ParallaxBg url="/images/default.jpg" />
       {/* <pre>{JSON.stringify(posts, null, 4)}</pre> */}
       <div className="container">
