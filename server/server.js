828f275093e411881f1b427b45c1c12ae980752c
Volodymyr <XoDeR@users.noreply.github.com>
2023-05-02 16:00:26 +0200

    Server: added socket.io init

--- server/server.js
+++ server/server.js
@@ -7,6 +7,14 @@ const morgan = require("morgan");
 require("dotenv").config();
 
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-type"],
+  },
+});
 
 mongoose
   .connect(process.env.DATABASE, {
@@ -19,12 +27,17 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [process.env.CLIENT_URL],
   })
 );
 
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
+io.on("connect", (socket) => {
+  console.log("Socket.IO", socket.id);
+});
+
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
+http.listen(port, () => console.log(`Server is running on port ${port}`));
